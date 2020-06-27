5d4975f5e161854b57eb849780e72d8a9a16c523
gksh7 <gksh1613@gmail.com>
2020-06-27 22:52:59 +0900

    first-commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('fetch', function(e) {
+  // ここは空でもOK
+})
+
+// ファイブラリのインポート
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+// // ファイルのキャッシュ
+// workbox.precaching.precacheAndRoute([
+//   {
+//     url: '/index.html',
+//     revision: '12345'
+//   },
+//   {
+//     url: '/app.css',
+//     revision: '12345'
+//   },
+//   { 
+// 　url: '/app.js',
+//     revision: '12345'
+//   },
+// ])

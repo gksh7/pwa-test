ba171ef93e9f28473abd4e3e09c10e3718921d1d
gksh7 <gksh1613@gmail.com>
2020-06-27 22:59:12 +0900

    sw.jsキャッシュ記述を追加

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@ self.addEventListener('fetch', function(e) {
 })
 
 // ファイブラリのインポート
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-// // ファイルのキャッシュ
-// workbox.precaching.precacheAndRoute([
-//   {
-//     url: '/index.html',
-//     revision: '12345'
-//   },
-//   {
-//     url: '/app.css',
-//     revision: '12345'
-//   },
-//   { 
-// 　url: '/app.js',
-//     revision: '12345'
-//   },
-// ])
+// ファイルのキャッシュ
+workbox.precaching.precacheAndRoute([
+  {
+    url: '/index.html',
+    revision: '12345'
+  },
+  {
+    url: '/app.css',
+    revision: '12345'
+  },
+  { 
+　url: '/app.js',
+    revision: '12345'
+  },
+])
